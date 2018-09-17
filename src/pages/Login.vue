3707bbe3f7f93285b05d3b5d784fb715883008a6
etechnidhi <nidhi_sharma@excellencetechnologies.in>
2018-09-17 13:24:56 +0530

    Initial Commit for all code fushed in master.

---
+++ src/pages/Login.vue
@@ -0,0 +1,17 @@
+<template>
+    <LoginForm/>
+</template>
+
+<script>
+import LoginForm from './../container/LoginForm.vue'
+export default {
+  name: 'Login',
+  components: {
+    LoginForm
+  }
+}
+</script>
+
+<style>
+
+</style>
